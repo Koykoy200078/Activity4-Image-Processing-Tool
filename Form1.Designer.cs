b1af8fe895d8457d79eda51c156cb013958e5747
Koykoy200078 <franc200078@gmail.com>
2025-06-03 11:05:55 +0800

    updated

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ImageProcessingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnGrayscale;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.Button btnBlur;
+
+        /// <summary>
+        /// Clean up resources.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnGrayscale = new System.Windows.Forms.Button();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.btnBlur = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+
+            // pictureBox
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+
+            // btnLoad
+            this.btnLoad.Location = new System.Drawing.Point(430, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(120, 30);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Image";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+
+            // btnGrayscale
+            this.btnGrayscale.Location = new System.Drawing.Point(430, 52);
+            this.btnGrayscale.Name = "btnGrayscale";
+            this.btnGrayscale.Size = new System.Drawing.Size(120, 30);
+            this.btnGrayscale.TabIndex = 2;
+            this.btnGrayscale.Text = "Grayscale";
+            this.btnGrayscale.UseVisualStyleBackColor = true;
+            this.btnGrayscale.Click += new System.EventHandler(this.btnGrayscale_Click);
+
+            // btnInvert
+            this.btnInvert.Location = new System.Drawing.Point(430, 92);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(120, 30);
+            this.btnInvert.TabIndex = 3;
+            this.btnInvert.Text = "Invert Colors";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+
+            // btnBlur
+            this.btnBlur.Location = new System.Drawing.Point(430, 132);
+            this.btnBlur.Name = "btnBlur";
+            this.btnBlur.Size = new System.Drawing.Size(120, 30);
+            this.btnBlur.TabIndex = 4;
+            this.btnBlur.Text = "Blur";
+            this.btnBlur.UseVisualStyleBackColor = true;
+            this.btnBlur.Click += new System.EventHandler(this.btnBlur_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(570, 330);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnGrayscale);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.btnBlur);
+            this.Name = "Form1";
+            this.Text = "Image Processing Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace ImageProcessingApp
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap loadedImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                loadedImage = new Bitmap(ofd.FileName);
+                pictureBox.Image = loadedImage;
+            }
+        }
+
+        private void btnGrayscale_Click(object sender, EventArgs e)
+        {
+            if (loadedImage == null) return;
+            Grayscale grayFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            pictureBox.Image = grayFilter.Apply(loadedImage);
+        }
+
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            if (loadedImage == null) return;
+            Invert invertFilter = new Invert();
+            pictureBox.Image = invertFilter.Apply(loadedImage);
+        }
+
+        private void btnBlur_Click(object sender, EventArgs e)
+        {
+            if (loadedImage == null) return;
+            GaussianBlur blurFilter = new GaussianBlur(4, 11);
+            pictureBox.Image = blurFilter.Apply(loadedImage);
+        }
+    }
+}
